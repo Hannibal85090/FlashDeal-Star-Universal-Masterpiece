5783c631549ecff78d76b09bce7c33b7a8d819de
Hannibal85090 <aldid220@gmail.com>
2026-05-07 00:30:29 +0100

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-# FlashDeal Star - Configuration System
-# استباق تعارض الإعدادات بجعلها ثابتة (Immutable)
-APP_NAME = "FlashDeal Star Universal"
-TOKEN_EXPIRY = 300
-SECURITY_LEVEL = "SOVEREIGN"
-VERSION = "1.0.0"
